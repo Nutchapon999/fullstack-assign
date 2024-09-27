4a030d19ff754b8265285fd6c66f13c2863567f5
Nutchapon999 <sr.nutchapon_st@tni.ac.th>
2024-09-27 21:11:43 +0700

    fix package

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,14 +1,15 @@
+import { config } from "dotenv";
+
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { config } from "dotenv";
 
 // *** Uncomment for unit test ***
 
-config({ path: ".env.local" });
+// config({ path: ".env.local" });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set.");
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL environment variable is not set.");
+// }
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
